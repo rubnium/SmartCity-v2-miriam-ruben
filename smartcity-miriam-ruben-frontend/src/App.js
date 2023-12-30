2c284650064d6b210fc09949365b0101cb6aaebc
rubnium <rubengomezvillegas@gmail.com>
2023-12-30 02:10:50 +0100

    Modificado Main y footer

--- smartcity-miriam-ruben-frontend/src/App.js
+++ smartcity-miriam-ruben-frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from './components/Main';
 import Navbar from "./components/navbar/Navbar";
 import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -12,6 +13,11 @@ function App() {
           <Routes>
               <Route path="/" element={<Main/>} />
           </Routes>
+          <footer className="footer-container"><p>
+            Desarrollado por Miriam Fernández Osuna y Rubén Gómez Villegas<br/>
+            UCLM Talavera de la Reina - Grado en Ingeniería Informática<br/>
+            Sistemas de Información Ubicuos 23/24
+          </p></footer>
       </div>
     </BrowserRouter>
   );
--- smartcity-miriam-ruben-frontend/src/components/Main.js
+++ smartcity-miriam-ruben-frontend/src/components/Main.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Card, CardContent, Grid, Typography } from '@mui/material';
+
+const cardStyle = {
+    margin: '10px',
+    width: 'calc(100% - 20px)', // Garantiza que ocupe el 100% menos el doble del margen
+  };
 
 export default function Main() {
     return (
-        <p>Hello world</p>
+        <Grid container rowSpacing={4.5} columnSpacing={2.75}>
+            {/* row 1 */}
+            <Grid item xs={12} sx={{ mb: -2.25, ml: '10px', mt: '10px' }}>
+                <Typography variant="h7">Dataset</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Card sx={{ minWidth: 275 }} style={cardStyle}>
+                    <CardContent>
+                        <Grid container spacing={3}>
+                            <Grid item xs={12}>
+                                <p>Hola mundo</p>
+                            </Grid>
+                            <Grid item xs={6} sm={4} md={3} lg={2}>
+                                <p>Holas</p>
+                            </Grid>
+                        </Grid>
+                    </CardContent>
+                </Card>
+                
+            </Grid>
+        </Grid>
     )
 }
