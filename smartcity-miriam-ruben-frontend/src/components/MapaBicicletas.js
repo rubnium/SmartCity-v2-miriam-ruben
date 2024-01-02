1a77014db0fbe4c97eed6b4576830ae5fbeac269
rubnium <miriam.fdez@alu.uclm.es>
2024-01-02 15:38:03 +0100

    Mapa Bicicletas casi funcional
    Co-Authored-By: rubnium <Ruben.Gomez10@alu.uclm.es>

--- smartcity-miriam-ruben-frontend/src/components/MapaBicicletas.js
+++ smartcity-miriam-ruben-frontend/src/components/MapaBicicletas.js
@@ -1,64 +1,86 @@
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
-import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
-import 'leaflet-defaulticon-compatibility';
 import { useEffect, useState } from 'react';
-import 'leaflet-polylinedecorator';
+import { MapContainer, useMap } from 'react-leaflet';
 import L from 'leaflet';
 
 import api from '../utils/api';
 import './ExampleMap.css';
 
+const madridCenter = [40.41692952216298, -3.700834722849244];
+const limitesMapa = L.latLngBounds(
+  [41.802697, -7.384931], 
+  [38.916897, -0.345458]
+);
+const marcadores = [];
 
-const obtenerMarcadores = async (fecha, hora, setData, setError) => {
+
+const obtenerMarcadores = async (fecha, hora, setError) => {
   try {
     const response = await api.get('/bicicletasAforo/'+ fecha +'/'+hora);
-    setData(response.data);
+    marcadores = response.data;
   } catch (error) {
     console.error('Error al obtener datos:', error);
     setError(error.message || 'Error en la solicitud');
   }
 };
 
+function UseMap() {
+  const map = useMap();
+  map.setView(madridCenter, 11);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+  map.setMaxBounds(limitesMapa);
+  map.options.bounceAtZoomLimits = false;
+
+  const canvas = L.canvas();
+  map.addLayer(canvas);
+
+  useEffect(() => {    
+    marcadores.forEach(marcador => {
+      const {lat, lon, bicicletas} = marcador;
+
+      L.circleMarker([lat, lon], {
+        renderer: canvas
+      }).addTo(map).bindPopup(bicicletas);   
+    });
+  }, [marcadores]);
+
+  L.circleMarker([48.85, 2.35], {
+  	renderer: canvas
+  }).addTo(map).bindPopup('marker 2');
+  return null;
+}
+
 const MapaBicicletas = (props) => {
   const { fecha, hora, contador } = props;
   const [fechaLocal, setFecha] = useState('');
-  const madridCenter = [40.41692952216298, -3.700834722849244];
-  const [marcadores, setMarcadores] = useState([]);
 
   const [error, setError] = useState(null);
 
+
   useEffect(() => {
     setFecha(fecha);
-    if (contador == 1) {
-      obtenerMarcadores(fecha, hora, setMarcadores, setError);
+    if (contador === 1) {
+      obtenerMarcadores(fecha, hora, setError);
     }
   }, [contador]);
 
   useEffect(() => {
-    obtenerMarcadores(fecha, hora, setMarcadores, setError);
-  
+    obtenerMarcadores(fecha, hora, setError);
   }, [hora]);
+
 
   return (
     <div>
       <p>Fecha: {fechaLocal}</p>
-      <p>Hora: {hora}</p>
+      <p>Hora: {hora} {marcadores.length}</p>
       <ul>
       {marcadores.map((marcador, index) => (
           <li>{marcador.id}, {marcador.lat}, {marcador.lon}, {marcador.bicicletas}</li>
       ))}
       </ul>
-      {/*
-      <MapContainer center={madridCenter} id="mapId" zoom={13}>
-        {marcadoresMetroLigero.map((marcador, index) => (
-          <Marker position={[marcador.lat, marcador.lon]}>
-            <Popup>Línea: {marcador.linea}<br/>Parada: {marcador.parada}</Popup>
-          </Marker>
-        ))}
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <MapContainer>
+        <UseMap />
       </MapContainer>
-    */}
     </div>
   );
 }
